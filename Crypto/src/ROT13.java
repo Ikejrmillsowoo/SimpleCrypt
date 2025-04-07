2bf42f1282f315710ad84e9900c732c20cfc3e0b
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-07 17:53:13 -0400

    completed

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,10 +1,14 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
+    List<String> unCryptFileArr = new ArrayList<>() {
+    };
 
     public static void main(String[] args) throws IOException {
 
@@ -13,7 +17,8 @@ public class ROT13  {
         ROT13 rot13 = new ROT13();
         rot13.readFileAndProcess(reader);
         BufferedReader reader2 = new BufferedReader(new FileReader("sonnet18.enc"));
-        System.out.println(rot13.compareFiles(reader, reader2));
+        System.out.println(rot13.compareFiles(reader,reader2));
+//       rot13.compareFiles(reader,reader2);
     }
 
     ROT13(Character cs, Character cf) {
@@ -24,15 +29,16 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-       char[] newArr = text.toLowerCase().toCharArray();
-        for (int i = 0; i< text.length(); i++){
-            if (newArr[i] >='a' && newArr[i] <='z') {
+        char[] newArr = text.toCharArray();
+        for (int i = 0; i < text.length(); i++) {
+            if (newArr[i] >= 'a' && newArr[i] <= 'z') {
                 newArr[i] = (char) ('a' + (newArr[i] - 'a' + 13) % 26);
+            }
+            if (newArr[i] >= 'A' && newArr[i] <= 'Z') {
+                newArr[i] = (char) ('A' + (newArr[i] - 'A' + 13) % 26);
             }
         }
         String str = new String(newArr);
-        str = str.substring(0, 1).toUpperCase() + str.substring(1);
-
         return str;
     }
 
@@ -41,7 +47,7 @@ public class ROT13  {
     }
 
     public String decrypt(String text) {
-        System.out.println(crypt(text));
+       // System.out.println(crypt(text));
         return crypt(text);
     }
 
@@ -52,40 +58,44 @@ public class ROT13  {
         for (int i = 0; i < arr.length; i++) {
             char ch = arr[i];
             if (ch >= 'A' && ch <= 'Z') {
-                arr[i] = (char) ('A' + (ch - 'A' + shift) % arr.length);;
+                arr[i] = (char) ('A' + (ch - 'A' + shift) % arr.length);
             }
         }
 
         String result = new String(arr);
-        System.out.println(result);
         return result;
     }
 
     public void readFileAndProcess(BufferedReader reader) throws IOException {
-//        BufferedReader reader = new BufferedReader(new FileReader("sonnet18.txt"));
-        try ( BufferedWriter writer = new BufferedWriter(new FileWriter("sonnet18.enc"))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("sonnet18.enc"))) {
             String line;
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
+                unCryptFileArr.add(line);
                 String newLine = encrypt(line);
                 writer.write(newLine);
                 writer.newLine();
-                System.out.println(newLine);
-        }
-        }
-    }
-
-    public boolean compareFiles(BufferedReader reader, BufferedReader reader2){
-        try {
-            String originalFile, alteredFile;
-            while ((originalFile = reader.readLine()) != null & (alteredFile = reader2.readLine()) != null) {
-                if (!originalFile.equals(alteredFile)) return false;
             }
-            return reader.readLine() == null && reader2.readLine() == null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
         }
-
     }
 
+    public boolean compareFiles(BufferedReader reader, BufferedReader reader2) {
+        List<String> deCryptFileArr = reader2.lines().toList();
+        ArrayList<String> decryptedLines = new ArrayList<>();
+        for(String line : deCryptFileArr){
+            decryptedLines.add(decrypt(line));
+        }
+
+        return decryptedLines.equals(unCryptFileArr);
+
+//        try {
+//            String originalLine, alteredLine;
+//            while ((originalLine = reader.readLine()) != null & (alteredLine = reader2.readLine()) != null) {
+//                if (originalLine.equals(decrypt(alteredLine))) return false;
+//            }
+//            return reader.readLine() == null && reader2.readLine() == null;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+    }
 }
